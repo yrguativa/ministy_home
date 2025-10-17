fc4514f13733bba5dba65ec86b872f1612c8cc23
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2025-10-16 23:21:57 -0500

    feat: integrate Tailwind CSS and animations into the Astro project
    - Updated `astro.config.mjs` to include Tailwind CSS and its Vite plugin.
    - Added Tailwind CSS and animation dependencies in `package.json` and `package-lock.json`.
    - Modified `index.astro` to enhance the page structure and include metadata.
    - Created `global.css` to import Tailwind CSS and the Midudev Tailwind animations plugin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yrguativa.github.io',
-    base: '/ministy_home',
+  site: 'https://yrguativa.github.io',
+  base: '/ministy_home',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
